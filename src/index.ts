614d32aa44cf006d2fae80b10eb29a9bf025bb64
mannkostir <agabatov7@gmail.com>
2026-02-14 19:05:06 +0600

    add export types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-export * from './transport-connector.module';
+export * from "./transport-connector.module";
+
+export {
+  Message,
+  MessageHandlerKey,
+} from "./decorators/message-handler.decorator";
+
+export { ConsumerProxy } from "./base/consumer-proxy";
+export { ProducerProxy } from "./base/producer-proxy";
+
+export { MessageType, MessageKey, MessageValue } from "./types/message.type";
+export { MessageOptions } from "./types/message-options.type";
+export { MessageFormat } from "./types/message-format.type";
+export { MessageErrorHandlingConfig } from "./types/message-error-handling.type";
+export { ConsumerSubscriptionParameters } from "./types/consumer-subscription-parameters.type";
+export {
+  TransportConnectorModuleOptions,
+  SchemaRegistryOptions,
+} from "./types/transport-connector-module-options.type";
