7dcbc04faa09ec8387b2004f84ecb7b829ef39b0
mannkostir <agabatov7@gmail.com>
2026-09-16 22:47:50 +0600

    add jest harness

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  projects: [
+    {
+      displayName: 'unit',
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      testMatch: ['<rootDir>/src/**/*.spec.ts'],
+    },
+    {
+      displayName: 'integration',
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      testMatch: ['<rootDir>/test/integration/**/*.int-spec.ts'],
+      testTimeout: 180000,
+    },
+  ],
+};
